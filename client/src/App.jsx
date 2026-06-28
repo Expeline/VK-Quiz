5b6e12ded6bfe68c099a38bae616cd52c997afcd
Expeline <kalashyan.artur@list.ru>
2026-06-29 01:14:23 +0300

    Add Vercel Analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import AppRouter from "./router/AppRouter";
 import { AuthProvider } from "./context/AuthContext/AuthContext.jsx";
 import { ThemeProvider } from "./context/ThemeContext/ThemeContext.jsx";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     return (
         <ThemeProvider>
             <AuthProvider>
                 <AppRouter />
+                <Analytics />
             </AuthProvider>
         </ThemeProvider>
     );
